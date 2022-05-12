1c7895109e0bf9b2261f8fc9dbf9440fd8b6b50c
Vinicius <viniciusflexa@gmail.com>
2022-05-11 21:01:12 -0300

    tailwind error

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+export function Header(){
+    return(
+        <>
+            <div className="w-auto h-20 bg-black text-white">
+                <div>
+                    <h1>ALL THINGS</h1>
+                </div>
+            </div>
+        </>
+    )
+}
