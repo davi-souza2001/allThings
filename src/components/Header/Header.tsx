caf3bfeb8a5573d3770cbbb0036d35822aa221df
Vinicius <viniciusflexa@gmail.com>
2022-05-13 09:32:21 -0300

    Router Updates

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,20 @@
 import { BookBookmark, UserCircle } from "phosphor-react";
+import { Link } from "react-router-dom";
 
 export function Header(){
     return(
         <div className="flex flex-row bg-black text-zinc-100 w-auto h-14 items-center justify-between">
-            <div className="flex items-center text-xl px-6 cursor-pointer">
+            <Link to="/" className="flex items-center text-xl px-6 cursor-pointer">
                 <BookBookmark className="w-6 h-6 mr-2" />
-                <a href="">ALL THINGS</a>
-            </div>
+                ALL THINGS
+            </Link>
 
             {/* Precisa fazer o modo mobile */}
             {/* Utilizar <DotsThreeVertical /> */}
             <div className="flex flex-row items-center text-lg px-6">
-                <a href="" className="px-6">Início</a>
+                <Link to="/" className="px-6">
+                    Início
+                </Link>
 
                 <div className="flex flex-row items-center cursor-pointer">
                     <p>Nome do Usuário</p>
