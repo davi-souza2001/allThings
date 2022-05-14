427078dace8ebefc7d7d312f3ad8fff86cea1eae
davi-souza2001 <davisouza2001dv@gmail.com>
2022-05-14 10:55:21 -0300

    up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,25 @@
 import { BookBookmark, DotsThreeVertical, UserCircle } from "phosphor-react";
 import { Link } from "react-router-dom";
 
-export function Header(){
-    return(
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuDivider,
+} from '@chakra-ui/react'
+
+export function Header() {
+    return (
         <div className="flex flex-row bg-black text-zinc-100 w-auto h-14 items-center justify-between">
             <Link to="/" className="flex items-center text-xl px-6 cursor-pointer">
                 <BookBookmark className="w-6 h-6 mr-2" />
                 ALL THINGS
             </Link>
 
-            {/* Precisa fazer o modo mobile */}
-            {/* Utilizar <DotsThreeVertical /> */}
             <div className="flex flex-rowitems-center text-lg px-6">
                 <Link to="/" className="px-6 hidden md:flex">
                     Início
@@ -21,7 +30,18 @@ export function Header(){
                     <UserCircle className='w-6 h-6 ml-2' weight="bold" />
                 </Link>
 
-                <DotsThreeVertical weight="bold" className="flex w-6 h-6 md:hidden"/>
+                <Menu>
+                    <MenuButton className="md:hidden">
+                        <DotsThreeVertical weight="bold" className="flex w-6 h-6 md:hidden" />
+                    </MenuButton>
+                    <MenuList>
+                        <MenuItem>Download</MenuItem>
+                        <MenuItem>Create a Copy</MenuItem>
+                        <MenuItem>Mark as Draft</MenuItem>
+                        <MenuItem>Delete</MenuItem>
+                        <MenuItem>Attend a Workshop</MenuItem>
+                    </MenuList>
+                </Menu>
             </div>
         </div>
     )
