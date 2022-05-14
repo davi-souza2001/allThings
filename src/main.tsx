e2f78c4a1c0545b981836fb40c2267165c5f9245
davi-souza2001 <davisouza2001dv@gmail.com>
2022-05-13 21:22:51 -0300

    up

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,10 @@ import App from './App'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 
-import { ChakraProvider } from '@chakra-ui/react';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ChakraProvider>
   </React.StrictMode>
 )
