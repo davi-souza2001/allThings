8ddf822e0fef89236f506f4f17160b600250326f
davi-souza2001 <davisouza2001dv@gmail.com>
2022-05-16 21:32:53 -0300

    add toast

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,19 @@
 import Idea from '../imgs/idea.png';
 import Thought from '../imgs/thought.png';
 
+import { ToastContainer, toast, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export function WidgetForm() {
+    const notify = () => toast.dark('🦄 Wow so easy!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
 
     return (
         <div className=" bg-zinc-800 p-4 relative rounded-2xl mb-4 flex flex-col item-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -9,9 +21,10 @@ export function WidgetForm() {
                 <span>Escreva sua nota</span>
             </div>
             <div className="w-full h-full flex items-center justify-center">
-                <div className="bg-zinc-700 w-32 h-28 flex items-center justify-center flex-col rounded-md cursor-pointer">
+                <div onClick={notify} className="bg-zinc-700 w-32 h-28 flex items-center justify-center flex-col rounded-md cursor-pointer">
                     <img src={Idea} alt="Icone para criar página" className="mb-4" />
                     <span>Criar página</span>
+                    <ToastContainer/>
                 </div>
             </div>
         </div>
