82a9ad845aec184c4b66c727243e438928d33b6a
davi-souza2001 <davisouza2001dv@gmail.com>
2022-07-20 20:27:16 -0300

    up

--- src/components/Modal/ModalCreatePage.tsx
+++ src/components/Modal/ModalCreatePage.tsx
@@ -109,169 +109,27 @@ export function ModalCreatePage(props: ModalCreatePageProps) {
 	return (
 		<div>
 			{modal === 'CreatePage' && (
-				<div className={`bg-[rgba(0,0,0,.5)]
-        w-screen h-screen
-        flex items-center justify-center
-        absolute inset-0
-        ${!props.open && 'hidden'}
-    `}>
+				<div className={`bg-[rgba(0,0,0,.5)] w-screen h-screen flex items-center justify-center absolute inset-0 ${!props.open && 'hidden'}`}>
 					<ToastContainer />
-
-					<div className={`
-      bg-[#393939]
-        flex items-center justify-start flex-col
-        w-screen sm:w-2/4 md:w-3/6 lg:w-2/5
-        h-4/5 md:h-4/5 lg:h-5/6
-        rounded-lg
-        transition-all
-     `}>
+					<div className='bg-[#242424] flex items-center justify-start flex-col w-screen sm:w-2/4 md:w-3/6 lg:w-2/5 h-4/5 md:h-4/5 lg:h-5/6 rounded-lg transition-all '>
 						<div onClick={props.close} className="w-full h-7 flex items-center justify-end">
 							<XCircle className="text-2xl w-7 h-7 bg-red-500 rounded-lg mr-3 mt-3 cursor-pointer" />
 						</div>
-						<div className={`
-          w-full h-10 md:h-14
-          flex items-center justify-center
-        `}>
-							<span className="font-semibold text-2xl xl:text-3xl">Página</span>
-						</div>
-						<div className={`
-           w-full h-12
-           flex items-center justify-center
-           mt-2
-        `}>
-							<input
-								type="text"
-								placeholder="Nome da página"
-								onChange={(e) => setNamePage(e.target.value)}
-								className={`
-                  md:w-80 lg:w-3/4 xl:h-12
-                  rounded-lg
-                  outline-none
-                  border-none
-                bg-[#4f4f4f]
-                  transition-all
-                border-zinc-600
-                focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none
-                  `}
-							/>
-						</div>
-						<div className={`
-           w-full h-24
-           flex items-center justify-center
-           mt-4
-        `}>
-							<span className="text-2xl font-semibold xl:text-3xl">Prioridade da página</span>
-						</div>
-						<div className={`
-           w-full h-32 xl:h-40 xl:mt-20
-           flex items-center justify-around
-        `}>
-							<div className="flex items-center justify-center flex-col cursor-pointer">
-								<span
-									onClick={() => setLevelPage("low")}
-									className={`
-                  bg-green-500
-                  text-green-500
-                    h-11 w-11
-                    flex items-center justify-center
-                    rounded-full
-                    xl:h-16 xl:w-16
-                    ${levelPage === 'low' && 'border-2 border-green-200'}
-                  `}>
-									Baixa
-								</span>
-								<span className="xl:text-xl xl:mt-5">
-									Baixa
-								</span>
-							</div>
-							<div className="flex items-center justify-center flex-col cursor-pointer">
-								<span
-									onClick={() => setLevelPage("medium")}
-									className={`
-                bg-yellow-300
-                text-yellow-300
-                  h-11 w-11
-                  flex items-center justify-center
-                  rounded-full
-                  xl:h-16 xl:w-16
-                  ${levelPage === 'medium' && 'border-2 border-yellow-100'}
-                  `}>
-									Média
-								</span>
-								<span className="xl:text-xl xl:mt-5">
-									Média
-								</span>
-							</div>
-							<div className="flex items-center justify-center flex-col cursor-pointer">
-								<span
-									onClick={() => setLevelPage("high")}
-									className={`
-                bg-red-500
-                text-red-500
-                  h-11 w-11
-                  flex items-center justify-center
-                  rounded-full
-                  xl:h-16 xl:w-16
-                  ${levelPage === 'high' && 'border-2 border-red-300'}
-                  `}>
-									Alta
-								</span>
-								<span className="xl:text-xl xl:mt-5">
-									Alta
-								</span>
-							</div>
-						</div>
-						<div className={`
-          w-full h-14 lg:h-2/5 lg:items-end
-          flex items-center justify-center
-        `}>
-							<button
-								onClick={createPage}
-								className={`
-                w-40 h-14 xl:h-14 xl:w-52
-              bg-brand-500
-                rounded-lg
-                text-xl font-semibold
-                cursor-pointer
-                lg:mb-10
-              hover:bg-brand-300
-                transition-all
-          `}>
-								{loading ? (
-									<CircularProgress isIndeterminate color='rgb(130 87 230)' />
-								)
-									:
-									'Criar'}
-							</button>
+						<div className='w-full h-10 md:h-14 flex items-center justify-center'>
+							<span className='font-semibold text-2xl xl:text-3xl'>Página</span>
 						</div>
 					</div>
 				</div >
 			)}
 
 			{modal === 'CreateNote' && (
-				<div className={`bg-[rgba(0,0,0,.5)]
-        w-screen h-screen
-        flex items-center justify-center
-        absolute inset-0
-        ${!props.open && 'hidden'}
-        `}>
-					<div className={`
-          bg-[#393939]
-            flex items-center justify-start flex-col
-            w-screen sm:w-2/4 md:w-3/6 lg:w-2/5
-            h-4/5 lg:h-96
-            rounded-lg
-            transition-all
-            `}>
+				<div className={`bg-[rgba(0,0,0,.5)] w-screen h-screen flex items-center justify-center absolute inset-0 ${!props.open && 'hidden'}`}>
+					<div className={`bg-[#393939] flex items-center justify-start flex-col w-screen sm:w-2/4 md:w-3/6 lg:w-2/5 h-4/5 lg:h-96 rounded-lg transition-all`}>
 						<ToastContainer />
 						<div onClick={props.close} className="w-full h-7 flex items-center justify-end">
 							<XCircle className="text-2xl w-7 h-7 bg-red-500 rounded-lg mr-3 mt-3 cursor-pointer" />
 						</div>
-						<div className={`
-                mt-3
-                w-full h-10
-                flex items-center justify-center
-            `}>
+						<div className='mt-3 w-full h-10 flex items-center justify-center'>
 							<p className="font-semibold text-2xl">Criar nota</p>
 						</div>
 						<div className={`
