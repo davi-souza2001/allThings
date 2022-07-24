3405428e5cafb30ab974147fbe8700f96ff4e006
davi-souza2001 <davisouza2001dv@gmail.com>
2022-07-24 10:22:51 -0300

    box create note complete

--- src/components/Modal/ModalCreatePage.tsx
+++ src/components/Modal/ModalCreatePage.tsx
@@ -23,8 +23,8 @@ export function ModalCreatePage(props: ModalCreatePageProps) {
 	const [namePage, setNamePage] = useState("")
 	const [levelPage, setLevelPage] = useState("low")
 
-	const [content, setContent] = useState("")
 	const [titleNote, setTitleNote] = useState("")
+	const [content, setContent] = useState("")
 	const [typeNote, setTypeNote] = useState<"note" | "image">("note")
 
 	function notifySucess(msg: string) {
@@ -89,24 +89,31 @@ export function ModalCreatePage(props: ModalCreatePageProps) {
 			return
 		}
 
-		// if (props.idPage) {
-		// 	setLoading(true)
-		// 	const data = {
-		// 		idPage: props.idPage,
-		// 		content,
-		// 		title: titleNote,
-		// 		type: typeNote
-		// 	}
+		const data = {
+			idPage: props.idPage,
+			content,
+			title: titleNote,
+			type: typeNote
+		}
 
-		// 	await Client.post('/note/create', data).then((res) => {
-		// 		setContent("")
-		// 		setTitleNote("")
-		// 		props.close()
-		// 		setChangeData(!changeData)
-		// 		setLoading(false)
-		// 		notifySucess('Nota criada com sucesso!')
-		// 	})
-		// }
+		if (props.idPage) {
+			setLoading(true)
+			const data = {
+				idPage: props.idPage,
+				content,
+				title: titleNote,
+				type: typeNote
+			}
+
+			await Client.post('/note/create', data).then((res) => {
+				setContent("")
+				setTitleNote("")
+				props.close()
+				setChangeData(!changeData)
+				setLoading(false)
+				notifySucess('Nota criada com sucesso!')
+			})
+		}
 	}
 
 	return (
@@ -114,7 +121,7 @@ export function ModalCreatePage(props: ModalCreatePageProps) {
 			{modal === 'CreatePage' && (
 				<div className={`bg-[rgba(0,0,0,.5)] w-screen h-screen flex items-center justify-center absolute inset-0 ${!props.open && 'hidden'}`}>
 					<ToastContainer />
-					<div className='bg-[#242424] flex items-center justify-start flex-col w-80 sm:w-2/4 md:w-3/6 lg:w-2/5 h-2/5 md:h-2/5 lg:h-3/6 rounded-lg transition-all xl:w-1/4'>
+					<div className='bg-[#242424] flex items-center justify-start flex-col w-80 h-2/5 sm:w-2/4 md:w-3/6 lg:w-2/5 md:h-2/5 lg:h-3/6 rounded-lg transition-all xl:w-1/4'>
 						<div onClick={props.close} className="w-full h-7 flex items-center justify-end">
 							<XCircle className="text-2xl w-7 h-7 bg-red-500 rounded-md mr-3 mt-3 cursor-pointer" />
 						</div>
@@ -154,7 +161,7 @@ export function ModalCreatePage(props: ModalCreatePageProps) {
 
 			{modal === 'CreateNote' && (
 				<div className={`bg-[rgba(0,0,0,.5)] w-screen h-screen flex items-center justify-center absolute inset-0 ${!props.open && 'hidden'}`}>
-					<div className={`bg-[#393939] flex items-center justify-start flex-col w-screen sm:w-2/4 md:w-3/6 lg:w-2/5 h-4/5 lg:h-96 rounded-lg transition-all`}>
+					<div className='bg-[#1b1b1b] w-80 h-4/6 flex items-center justify-start flex-col sm:w-2/4 md:w-3/6 lg:w-2/5  rounded-lg transition-all'>
 						<ToastContainer />
 						<div onClick={props.close} className="w-full h-7 flex items-center justify-end">
 							<XCircle className="text-2xl w-7 h-7 bg-red-500 rounded-lg mr-3 mt-3 cursor-pointer" />
@@ -162,86 +169,44 @@ export function ModalCreatePage(props: ModalCreatePageProps) {
 						<div className='mt-3 w-full h-10 flex items-center justify-center'>
 							<p className="font-semibold text-2xl">Criar nota</p>
 						</div>
-						<div className={`
-                mt-5
-                w-full h-8
-                flex items-center justify-center
-            `}>
-							<input type="text"
-								placeholder="Título da nota"
-								onChange={(e) => setTitleNote(e.target.value)}
-								className={`
-                  md:w-80 lg:w-3/4 xl:h-12
-                  rounded-lg
-                  outline-none
-                  border-none
-                bg-[#4f4f4f]
-                  transition-all
-                border-zinc-600
-                focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none
-                  `} />
-						</div>
-						<div className={`
-                mt-5
-                w-full h-48
-                flex items-center justify-center
-            `}>
-							<textarea
-								placeholder="Conteúdo da nota"
-								onChange={(e) => setContent(e.target.value)}
-								className={`
-                resize-none
-              bg-[#4f4f4f]
-                h-3/4 w-3/4 sm:h-20 xl:h-full
-                border-none
-                outline-none
-                rounded-lg
-              `}
-							/>
-						</div>
-						<div className={`
-                mt-5
-                w-full h-16
-                flex items-center justify-center
-            `}>
-							<select
-								onChange={(e) => setTitleNote(e.target.value)}
-								className={`
-                mb-5
-                w-48 md:w-80
-                bg-[#4f4f4f]
-                border-none
-                outline-none
-                rounded-lg
-                focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none
-              `}>
-								<option value="note">Nota</option>
-								<option value="image">Imagem</option>
-							</select>
-						</div>
-						<div className={`
-                mt-5
-                w-full h-16
-                flex items-center justify-center
-            `}>
-							<button
-								className={`
-                w-40 h-14 xl:h-14 xl:w-52
-              bg-brand-500
-                rounded-lg
-                text-xl font-semibold
-                cursor-pointer
-                lg:mb-10
-              hover:bg-brand-300
-                transition-all
-          `}>
-								{loading ? (
-									<CircularProgress isIndeterminate color='rgb(130 87 230)' />
-								)
-									:
-									'Criar'}
-							</button>
-						</div>
+						<form onSubmit={(e) => createNote(e)}>
+							<div className='h-14 w-72 flex items-center justify-around mt-2 bg-[#2b2b2b] rounded-md xl:w-[500px]'>
+								<Pen className='text-xl' />
+								<input
+									className='h-10 w-56 bg-[#2f2f2f] border-0 outline-none rounded-md xl:h-12 xl:w-[420px]'
+									placeholder="Add title"
+									type="text"
+									onChange={(e) => setTitleNote(e.target.value)}
+								/>
+							</div>
+							<div className='h-36 w-72 flex items-center justify-around mt-2 bg-[#2b2b2b] rounded-md xl:w-[500px]'>
+								<Pen className='text-xl' />
+								<textarea
+									placeholder="Conteúdo da nota"
+									onChange={(e) => setContent(e.target.value)}
+									className='resize-none bg-[#2f2f2f] h-3/4 w-3/4 border-none outline-none rounded-lg xl:w-[420px]'
+								/>
+							</div>
+							<div className='h-14 w-72 flex items-center justify-around mt-4 bg-[#2b2b2b] rounded-md xl:w-[500px]'>
+								<Bag className='text-xl' />
+								<select onChange={(e) => setLevelPage(e.target.value)} className='h-10 w-56 bg-[#2f2f2f] border-0 outline-none rounded-md xl:w-[420px]'>
+									<option value='note'>Note</option>
+									<option value='image'>Image</option>
+								</select>
+							</div>
+							<div className='mt-5 w-full h-16 flex items-center justify-center'>
+								<button
+									disabled={loading && true}
+									className='w-40 h-14 xl:h-14 xl:w-52 bg-brand-500 rounded-lg text-xl font-semibold cursor-pointer lg:mb-10 hover:bg-brand-300 transition-all lg:mt-20 xl:mt-[20vh]'>
+									{loading ? (
+										<CircularProgress isIndeterminate color='rgb(130 87 230)' />
+									)
+										:
+										'Criar'}
+								</button>
+							</div>
+						</form>
+
 					</div>
 				</div>
 			)}
